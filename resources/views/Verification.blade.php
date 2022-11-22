e46a5f933aecd53f417b2cb2234ba252cd6addfb
chbani <chbani@gmail.com>
2022-11-22 13:10:33 +0100

    'jj'

--- resources/views/Verification.blade.php
+++ resources/views/Verification.blade.php
@@ -13,27 +13,40 @@
               referrerpolicy="no-referrer" />
     <!-- end font awesome -->
     <!-- start css  -->
-    <link rel="stylesheet" href="normalize/normalize.css">
+    <link rel="stylesheet" href="{{ url('normalize/normalize.css') }}">
+    <link href="{{ url('css/Bootstrap.min.css') }}" rel="stylesheet">
     @vite(['resources/css/Verification.css'])
     <!-- end css  -->
 </head>
 <body>
     <div class="verification">
-        <div class="image">
-            <i class="fa-solid fa-envelope-open-text"></i>
-            <img src="image/background_email.png" alt="background-email">
-        </div>
-        <div>
-            <h1>Verify your email address</h1>
-        </div>
-        <div>
-            <p>We sent verification code to
-                <span>email@gmail.com</span>
-            </p>
-            <p> Please check your inbox and enter the code below</p>
-        </div>
+        <div class='verifi'>
+            <div class="image">
+                <i class="fa-solid fa-envelope-open-text"></i>
+                <img src="image/background_email.png" alt="background-email">
+            </div>
+            <div class='Verify'>
+                <h1>Verify your email address</h1>
+            </div>
+            <div class="message">
+                <p>We sent verification code to
+                    <span>email@gmail.com</span>
+                </p>
+                <p> Please check your inbox and enter the code below</p>
+            </div>
+            <form action="">
+                <div class="input">
+                    <input type="text">
+                    <input type="text">
+                    <input type="text">
+                    <input type="text">
+                </div>
+                <div>
+                    <button type="button" class="btn btn-success" >&#10003; Verify account</button>
+                </div>
+            </form>
 
-       
+        </div>
     </div>
     <!-- start js  -->
     <script src="js/jquery/jquery.js"></script>
