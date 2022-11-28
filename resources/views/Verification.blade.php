b54b0a9a5404fd47391a4eae48ab8b682ffd4c80
chbani <chbani@gmail.com>
2022-11-28 01:08:15 +0100

    sdfsdfsdf

--- resources/views/Verification.blade.php
+++ resources/views/Verification.blade.php
@@ -23,6 +23,11 @@
         <div class='verifi'>
             <div class="image">
                 <i class="fa-solid fa-envelope-open-text"></i>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-plus"></i>
                 <img src="image/background_email.png" alt="background-email">
             </div>
             <div class='Verify'>
