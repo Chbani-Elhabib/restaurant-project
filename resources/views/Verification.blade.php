ca33f38bd6d3c789e006f49ad57e602ffe3e0341
chbani <chbani@gmail.com>
2022-11-28 16:58:22 +0100

    fghjkl;'

--- resources/views/Verification.blade.php
+++ resources/views/Verification.blade.php
@@ -23,11 +23,27 @@
         <div class='verifi'>
             <div class="image">
                 <i class="fa-solid fa-envelope-open-text"></i>
-                <i class="fa-solid fa-plus"></i>
-                <i class="fa-solid fa-plus"></i>
-                <i class="fa-solid fa-plus"></i>
-                <i class="fa-solid fa-plus"></i>
-                <i class="fa-solid fa-plus"></i>
+                <div>
+                    <i class="fa-solid fa-plus"></i>
+                    <i class="fa-solid fa-plus"></i>
+                    <i class="fa-solid fa-plus"></i>
+                    <i class="fa-solid fa-plus"></i>
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+                <div>
+                    <i class="fa-regular fa-circle"></i>
+                    <i class="fa-regular fa-circle"></i>
+                    <i class="fa-regular fa-circle"></i>
+                </div>
+                <div>
+                    <i class="fa-solid fa-star-of-life"></i>
+                    <i class="fa-solid fa-star-of-life"></i>
+                </div>
+                <div>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
                 <img src="image/background_email.png" alt="background-email">
             </div>
             <div class='Verify'>
